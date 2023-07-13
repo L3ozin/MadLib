f1c244fa03c853e0e464710f1d96dc93af8e4679
L3ozin <leonardopradosuzin@gmail.com>
2023-07-12 21:08:47 -0300

    Primeiro commit

---
+++ projeto.py
@@ -0,0 +1,9 @@
+Pessoa = input("Diga o nome de uma pessoa: ")
+adj = input("Adjetivo: ")
+verbo1 = input("Verbo: ")
+verbo2 = input("Mais um verbo: ")
+
+madlib = f"Tenho um amigo que se chama {Pessoa}, ele é muito {adj},\
+ quando eu estou com ele gosto de {verbo1} e {verbo2}."
+
+print(madlib)
